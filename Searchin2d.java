d370c78c53d37ea49b858ffa8c99fa4128bfa55a
kunalwvm <107076313+kunalwvm@users.noreply.github.com>
2022-07-14 00:26:00 -0400

    Update Searchin2d.java

--- Searchin2d.java
+++ Searchin2d.java
@@ -1,2 +1,28 @@
-public class Searchin2d {
+// time complexity 0(m+n)
+// space complexity 0(1)
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+     if(matrix == null || matrix.length==0)
+         return false;
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int r=0;
+        int c=n-1;
+         while(r<m && c>=0)
+         {
+            if(matrix[r][c]<target)
+            {
+                r++;
+            }
+             else if(matrix[r][c]>target)
+             {
+                 c--;
+             }
+             else
+             {
+                 return true;
+             }
+         }
+        return false;
+    }
 }
